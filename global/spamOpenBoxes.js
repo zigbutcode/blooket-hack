f559a45e34118b476a4701fcf93e428e95912912
glizzz_y <73669084+glixzzy@users.noreply.github.com>
2022-01-01 16:31:01 -0600

    update

--- global/spamOpenBoxes.js
+++ global/spamOpenBoxes.js
@@ -1,6 +1,4 @@
-function sleep(ms) {
-    return new Promise(r => setTimeout(r, ms))
-};
+const sleep = (ms) => new Promise(r => setTimeout(r, ms));
 
 (async () => {
     const box = prompt('What box do you want to open? (EXAMPLE: Space)');
@@ -16,21 +14,32 @@ function sleep(ms) {
     });
     const data = await response.json();
 
-    for (let i = 0; i < amount; i++) {
-        await sleep(500)
-        fetch('https://api.blooket.com/api/users/unlockblook', {
-            method: "PUT",
-            headers: {
-                "accept": "application/json, text/plain, */*",
-                "accept-language": "en-US,en;q=0.9,ru;q=0.8",
-            },
-            credentials: "include",
-            body: JSON.stringify({
-                box: box,
-                name: data.name,
-            }),
-        }).then(response => response.json()).then(data => {
-            console.log('Unlocked blook: ' + data.unlockedBlook);
-        });
-    };
+    (new Promise(async (resolve, reject) => {
+        var blooks = [];
+
+        for (let i = 0; i < amount; i++) {
+            await sleep(500)
+            fetch('https://api.blooket.com/api/users/unlockblook', {
+                method: "PUT",
+                headers: {
+                    "accept": "application/json, text/plain, */*",
+                    "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+                },
+                credentials: "include",
+                body: JSON.stringify({
+                    box: box,
+                    name: data.name,
+                }),
+            }).then(r => r.json()).then(r => blooks.push(r.unlockedBlook));
+        };
+
+        resolve(blooks);
+
+    })).then(blooks => {
+        let count = {};
+
+        blooks.forEach((i) => count[i] = (count[i] || 0) + 1);
+        
+        alert(`Results:\n` + Object.entries(count).map((x) => `    ${x[1]} ${x[0]}`).join(`\n`));
+    });
 })();
