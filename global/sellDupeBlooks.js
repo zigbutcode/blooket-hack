b1912e869daa3305e88a08458d2de6be61cd4f82
glizzz_y <73669084+glixzzy@users.noreply.github.com>
2022-01-01 15:07:07 -0600

    new update v3

--- global/sellDupeBlooks.js
+++ global/sellDupeBlooks.js
@@ -1,17 +1,25 @@
-async function getName(authToken) {
-    const response = await fetch('https://api.blooket.com/api/users/verify-token?token=JWT+' + authToken);
+async function getName() {
+    const response = await fetch('https://api.blooket.com/api/users/verify-token', {
+        method: "GET",
+        headers: {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+        },
+        credentials: "include"
+    });
     const data = await response.json();
 
     return data.name;
 };
 
+
 async function getBlooks(blooketName) {
     const response = await fetch('https://api.blooket.com/api/users/blooks?name=' + blooketName, {
         headers: {
-            "referer": "https://www.blooket.com/",
-            "content-type": "application/json",
-            "authorization": localStorage.token
-        }
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+        },
+        credentials: "include"
     });
     const data = await response.json();
 
@@ -19,9 +27,7 @@ async function getBlooks(blooketName) {
 };
 
 async function sellDupeBlooks() {
-    const myToken = localStorage.token.split('JWT ')[1];
-    const blooketName = await getName(myToken);
-    const blooks = await getBlooks(blooketName);
+    const blooks = await getBlooks(await getName());
     const x = Object.entries(blooks).filter(i => i[1] > 1).map(i => [i[0], i[1] - 1]);
 
     for (const [blook, val] of x) {
@@ -30,11 +36,11 @@ async function sellDupeBlooks() {
             headers: {
                 "referer": "https://www.blooket.com/",
                 "content-type": "application/json",
-                "authorization": localStorage.token
             },
+            credentials: "include",
             body: JSON.stringify({
                 blook: blook,
-                name: blooketName,
+                name: await getName(),
                 numSold: val
             }),
         });
